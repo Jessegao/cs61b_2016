2c8f1a5b3585dc0a80ec4ecef7fb27e423b4bfe2
Jesse <jessegao12@gmail.com>
2016-02-04 02:05:35 -0800

    backup day one

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -1,5 +1,4 @@
-public class ArrayDeque<Item>
-{
+public class ArrayDeque<Item>{
 	private Item[] items;//vars from Hug's code
 	private int size;
 	private int front;
@@ -23,142 +22,127 @@ public class ArrayDeque<Item>
     	last = size-1;
     }
 
-    public void addFirst(Item thing)
-    {
-    	if(size == items.length)
-    	{
+    public void addFirst(Item thing){
+    	if(size == items.length){
     		resize();
     	}
-    	if(items[front] != null)
-    	{
-    		moveFront(-1);
-    	}
+    	moveFront(-1);
     	items[front] = thing;
         size++;
     }
 
     //adjusts front for you
     //i can only be 1 or -1
-    private void moveFront(int i)
-    {
-    	if(front + i >= size)
-    	{
+    private void moveFront(int i){
+    	if(front + i >= items.length){
     		front = 0;
     	}
-    	else if(front + i < 0)
-    	{
-    		front = size-1;
+    	else if(front + i < 0){
+    		front = items.length-1;
     	}
-    	else
-    	{
+    	else{
     		front += i;
     	}
     }
 
     private void moveLast(int i)
     {
-    	if(last + i >= size)
-    	{
+    	if(last + i >= items.length){
     		last = 0;
     	}
-    	else if(last + i < 0)
-    	{
-    		last = size-1;
+    	else if(last + i < 0){
+    		last = items.length-1;
     	}
-    	else
-    	{
+    	else{
     		last += i;
     	}
     }
 
 
-    public void addLast(Item thing)
-    {
-        if(size == items.length)
-    	{
+    public void addLast(Item thing){
+        if(size == items.length){
     		resize();
     	}
-    	if(items[last] != null)
-    	{
+    	if(items[last] != null){
     		moveLast(1);
     	}
     	items[last] = thing;
         size++;
     }
 
-    public boolean isEmpty()
-    {
+    public boolean isEmpty(){
         return(size == 0);
     }
 
-    public int size()
-    {
+    public int size(){
         return size;
     }
 
-    public void printDeque()
-    {
-        if(size == 0)
-        {
+    public void printDeque(){
+        if(size == 0){
             return;
         }
+        else{
+        	for(int i = front; i < items.length; i++){
+        		if(items[i]!=null){
+        			System.out.print(items[i].toString() + " ");
+        		}
+        	}
 
-        for(int i = front; i < items.length; i++)
-        {
-            System.out.print(items[i].toString() + " ");
-        }
-
-        for(int i = 0; i < last; i++)
-        {
-            System.out.print(items[i].toString() + " ");
-        }
-
-        System.out.print(items[last].toString() + "\n");
+       	 	for(int i = 0; i <= last; i++){
+            	if(items[i]!=null){
+        			System.out.print(items[i].toString() + " ");
+        		}
+        	}
+  		}	
     }
 
-    public Item removeFirst()
-    {
-        if(isEmpty())
-        {
+    private void downsize(){
+    	if(size*4<items.length && items.length>=16){
+    		Item[] a = (Item[]) new Object[size*RFACTOR];
+    		System.arraycopy(items, front, a, 0, items.length-front);
+    		System.arraycopy(items, 0, a, items.length-front, front+1);
+    		items = a;
+    		front = 0;
+    		last = size-1;
+    	}
+    }
+
+    public Item removeFirst(){
+        if(isEmpty()){
             return null;
         }
-        else
-        {
+        else{
             Item r = items[front];//to be deleted
 
             items[front] = null;
             moveFront(1);
 
             size--;
+            downsize();
             return r;
-
         }
     }
 
-    public Item removeLast()
-    {
-        if(isEmpty())
-        {
+    public Item removeLast(){
+        if(isEmpty()){
             return null;
         }
-        else
-        {
+        else{
             Item r = items[last];//to be deleted
             items[last] = null;
             moveLast(-1);
             size--;
+            downsize();
             return r;
         }
     }
 
-    public Item get(int index)
-    {
-        if(index>size || index < 0)
-        {
+    public Item get(int index){
+        if(index>size || index < 0){
             return null;
         }
-        else
-        {
+        else{
             return items[index + front];
         }
     }
--- proj1/LinkedListDeque.java
+++ proj1/LinkedListDeque.java
@@ -1,12 +1,10 @@
-public class LinkedListDeque<GenericThing>
-{
-    public class Node
-    {
+public class LinkedListDeque<Item>{
+    private class Node{
         public Node previous;
-        public GenericThing item;     /* Basically this is Hug's node class with a previous link */
+        public Item item;     /* Basically this is Hug's node class with a previous link */
         public Node next;
 
-        public Node(Node p, GenericThing i, Node h) {
+        public Node(Node p, Item i, Node h) {
             previous = p;
             item = i;
             next = h;
@@ -16,68 +14,122 @@ public class LinkedListDeque<GenericThing>
     private Node sentinel; /* this is Hug's instance vars also */
     private int size;
     private Node last;
+    //sentinel.next is always front
 
-    public LinkedListDeque()
-    {
-        sentinel = Node(null, null, null);
+    public LinkedListDeque(){
+        sentinel = new Node(null, null, null);
         size = 0;
         last = sentinel;
+        sentinel.previous = sentinel;
+        sentinel.next = sentinel;
     }
 
-    public LinkedListDeque(GenericThing thing)
-    {
-        sentinel = Node(null, null, Node(sentinel, thing, sentinel));
-        sentinel.previous = sentinel.next;
-        last = sentinel.next;
-        size = 1;
-    }
-
-    public void addFirst(GenericThing thing)
-    {
-        GenericThing oldNode = sentinel.next;
-        sentinel.next = Node(sentinel, thing, oldNode);
+    public void addFirst(Item thing){
+        Node oldNode = sentinel.next;
+        //last needs to account for first add
+        boolean empty = isEmpty();
+        //set the proper pointers for the new node
+        sentinel.next = new Node(sentinel, thing, oldNode);
+        //set pointer for oldnode
+        oldNode.previous = sentinel.next;
+        //special case for if the list was originally empty
+        if(isEmpty()){
+            last = sentinel.next;
+            sentinel.previous = last;
+        }
         size++;
     }
 
-    public void addLast(GenericThing thing)
-    {
-        last.next = Node(last, thing, sentinel);
+    public void addLast(Item thing){
+        last.next = new Node(last, thing, sentinel);
+        last = last.next;
+        sentinel.previous = last;
         size++;
     }
 
-    public boolean isEmpty()
-    {
+    public boolean isEmpty(){
         return(size == 0);
     }
 
-    public int size()
-    {
+    public int size(){
         return size;
     }
 
-    public void printDeque()
-    {
-        Node tracker = this;
-        for(int i = size; i>1; i--)
-        {
+    public void printDeque(){
+        Node tracker = sentinel.next;
+        if(size == 0){
+            return;
+        }
+
+        for(int i = 0; i < size-1; i++){
             System.out.print(tracker.item.toString() + " ");
             tracker = tracker.next;
         }
-        System.out.print(tracker.item.toString());
+        System.out.print(tracker.item.toString() + "\n");
     }
 
-    public GenericThing removeFirst()
-    {
-        if(isEmpty())
-        {
-            return;
+    public Item removeFirst(){
+        if(isEmpty()){
+            return null;
         }
-        else
-        {
-            r = sentinel.next;
-            sentinel.next = sentinel.next.next;
-            return r;
+        else{
+            Node r = sentinel.next;//to be deleted
+
+            sentinel.next = sentinel.next.next;//removes sentinel references to r
+            sentinel.next.previous = sentinel;
+
+            r.previous = null;//isolates r
+            r.next = null;
+
+            size--;
+            return r.item;
         }
     }
-    
+
+    public Item removeLast(){
+        if(isEmpty()){
+            return null;
+        }
+        else{
+            Node r = last;
+            last = r.previous;
+            r.previous = null;
+            r.next = null;
+            last.previous.next = sentinel;
+            sentinel.previous = last;
+            size--;
+            return r.item;
+        }
+    }
+
+    public Item get(int index){
+        if(index>size || index < 0){
+            return null;
+        }
+        else{
+            Node g = sentinel.next;
+            while(index>0){
+                g = g.next;
+                index--;
+            }
+            return g.item;
+        }
+    }
+
+    private Item helper(int index, Node n){
+        if(index == 0)
+            return n.item;
+        else
+            return helper(index-1, n.next);
+    }
+
+    public Item getRecursive(int index){
+        if(index>size || index < 0){
+            return null;
+        }
+        else{
+        Node n = sentinel.next;
+        return helper(index, n);
+        }
+    }
 }
