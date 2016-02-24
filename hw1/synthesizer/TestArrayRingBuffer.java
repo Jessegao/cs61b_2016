8449642c720589da2abed35ec7fd5e6fe619bd80
Jesse <jessegao12@gmail.com>
2016-02-24 02:54:24 -0800

    done

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -8,8 +8,15 @@ import static org.junit.Assert.*;
 
 public class TestArrayRingBuffer {
     @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+    public void testEnqueueAndDequeue() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<Integer>(10);
+        for(int i = 0; i<10; i++){
+            arb.enqueue(i);
+        }
+        for (int i = 0; i<10; i++) {
+            int first = arb.dequeue();
+            assertEquals(i+1, first);
+        }
     }
 
     /** Calls tests for ArrayRingBuffer. */
